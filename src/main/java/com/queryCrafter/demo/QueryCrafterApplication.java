a0659325ef237b5102677dec1ebb8b79b381116b
Alisson Ferreira <alisson@virtos.com.br>
2025-05-19 14:22:02 -0300

    primeiro commit

---
+++ src/main/java/com/queryCrafter/demo/QueryCrafterApplication.java
@@ -0,0 +1,13 @@
+package com.queryCrafter.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryCrafterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryCrafterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/queryCrafter/demo/QueryCrafterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queryCrafter.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryCrafterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
